ddd1b1b317bc2e58eb6b6ccc32a7e665824e63d1
Andrew Natwaluma <andrewnatwaluma@gmail.com>
2025-09-21 12:05:40 +0300

    Create supabaseClient.js

---
+++ src/utils/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Using the provided FUMI Supabase project details
+const supabaseUrl = "https://iaenttkokcxtiauzjtgw.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhZW50dGtva2N4dGlhdXpqdGd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NDQ2NDksImV4cCI6MjA3MzQyMDY0OX0.u6ZBX-d_CTNlA94OM7h2JerNpmhuHZxYSXmj0OxRhRI"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
